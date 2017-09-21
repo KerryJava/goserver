27761723b98a8e794013def3c1faddb7ca8c3dfd
jet <qq471723562@126.com>
2017-09-21 20:53:53 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION="1.0"
+COMMIT="abcdefghijklmnopg"
+TARGET="main.go"
+
+go build  -gcflags "-N -l" -ldflags "-X other.VERSION=1.0.0 -X gpxj/other.VERSION=1.0.3 -X main.VERSION=1.0.0 -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'"  -v -o "server-${VERSION}-${COMMIT:0:7}" $TARGET 
+
