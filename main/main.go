60a1aed3493999a5a5324549d524b149ed2479ea
jetnet <qq471723562@126.com>
2017-09-22 13:22:25 +0800

    add httphandel func

--- main/main.go
+++ main/main.go
@@ -6,6 +6,7 @@ import "flag"
 import "github.com/KerryJava/goserver/base"
 import "github.com/KerryJava/goserver/other"
 import "net/http"
+import "strings"
 
 //import "log"
 import "github.com/golang/glog"
@@ -35,6 +36,7 @@ func main() {
 	s.RegisterService(new(base.Base), "")
 	s.RegisterService(new(other.Other), "")
 	http.Handle("/", s)
+	http.HandleFunc("/hello/", sayhelloName)
 
 	listenAddr := "0.0.0.0:8082"
 	e := http.ListenAndServe(listenAddr, nil)
@@ -48,4 +50,19 @@ func init() {
 	fmt.Println("init main ..........")
 	fmt.Printf("%s\n%s\n%s\n", VERSION, BUILD_TIME, GO_VERSION)
 
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析参数, 默认是不会解析的
+	fmt.Println(r.Form) //这些是服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	/*
+		fmt.Println(r.Form["url_long"])
+		for k, v := range r.Form {
+			fmt.Println("key:", k)
+			fmt.Println("val:", strings.Join(v, ""))
+		}
+	*/
+	fmt.Fprintf(w, "Hello ") //输出到客户端的信息
 }
