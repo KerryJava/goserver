8aa966c3635904b205d48e3c7169807a4e3f9bd7
jet <qq471723562@126.com>
2018-01-25 16:28:44 +0800

    add middleware and ORM

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 VERSION="1.0"
-COMMIT="abcdefghijklmnopg"
-TARGET="main.go"
+TARGET="main/main.go"
+PREFIX="github.com/KerryJava/goserver"
+echo "GOPATH is $GOPATH"
+echo "git 状态"
+git log --stat -n 1
 
-go build  -gcflags "-N -l" -ldflags "-X other.VERSION=1.0.0 -X gpxj/other.VERSION=1.0.3 -X main.VERSION=1.0.0 -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'"  -v -o "server-${VERSION}-${COMMIT:0:7}" $TARGET 
+COMMIT=$(git log | head -n 1 | cut -d ' ' -f 2)
+echo $COMMIT
+
+FLAGS=" -X '${PREFIX}/other.VERSION=${VERSION}' -X '${PREFIX}/other.BUILD_TIME=`date`' -X '${PREFIX}/other.GO_VERSION=`go version`' -X '${PREFIX}/other.COMMIT=${COMMIT}' \
+  	-X main.VERSION=${VERSION} -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'"
+#FLAGS="-X main.VERSION=1.0.0"
+
+go build -gcflags "-N -l" \
+       	-ldflags "${FLAGS}" \
+	-v -o "server-${VERSION}-${COMMIT:0:7}" \
+	$TARGET 
 
---
+++ main/main.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+import "flag"
+
+import "github.com/KerryJava/goserver/base"
+import "github.com/KerryJava/goserver/other"
+import "net/http"
+import "strings"
+
+//import "log"
+import "github.com/golang/glog"
+import "github.com/gorilla/rpc/v2"
+import "github.com/gorilla/rpc/v2/json2"
+
+type Main struct {
+}
+
+var (
+	VERSION    string
+	BUILD_TIME string
+	GO_VERSION string
+)
+
+func main() {
+
+	flag.Parse()
+
+	defer glog.Flush()
+
+	fmt.Println("welcome to goserver")
+	glog.Info("hello golang")
+
+	s := rpc.NewServer()
+	s.RegisterCodec(json2.NewCustomCodec(&rpc.CompressionSelector{}), "application/json")
+	s.RegisterService(new(base.Base), "")
+	s.RegisterService(new(other.Other), "")
+	http.Handle("/", s)
+	http.HandleFunc("/hello/", sayhelloName)
+
+	listenAddr := "0.0.0.0:8082"
+	e := http.ListenAndServe(listenAddr, nil)
+
+	if e != nil {
+		fmt.Println(e)
+	}
+}
+
+func init() {
+	fmt.Println("init main ..........")
+	fmt.Printf("%s\n%s\n%s\n", VERSION, BUILD_TIME, GO_VERSION)
+
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析参数, 默认是不会解析的
+	fmt.Println(r.Form) //这些是服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	/*
+		fmt.Println(r.Form["url_long"])
+		for k, v := range r.Form {
+			fmt.Println("key:", k)
+			fmt.Println("val:", strings.Join(v, ""))
+		}
+	*/
+	fmt.Fprintf(w, "Hello ") //输出到客户端的信息
+}
